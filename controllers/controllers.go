05c5e6bc34bd4dd779174d60da497b17e3daa59e
rs <raihansamhari@protonmail.com>
2022-04-18 12:49:45 +0700

    add /about

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -8,8 +8,17 @@ import (
 )
 
 const (
+	aboutTemplateName = "about.html"
 	statsTemplateName = "stats.html"
 )
+
+func About (c *server.Context) {
+	if c.TryCache(aboutTemplateName) {
+		return;
+	}
+
+	c.Cache(http.StatusOK, aboutTemplateName)
+}
 
 func Stats(c *server.Context) {
 	if c.TryCache(statsTemplateName) {
